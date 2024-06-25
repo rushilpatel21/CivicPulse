a833377a0ca7b90a0257f0e715585d5aeed4ced0
rushilpatel21 <rushilpatel210@gmail.com>
2024-06-26 02:25:50 +0530

    Trying to change the original name to have same name uploads

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ const storage = multer.diskStorage({
     cb(null, 'uploads/');
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + '-' + file.originalname);
   },
 });
 
